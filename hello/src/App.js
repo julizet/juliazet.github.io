8b127f5e3bc9247b446dfaf8120d676cc38a2caa
julizet <juliazet@gmx.de>
2021-03-07 12:21:46 +0100

    new: Links & Routes to About & Contact

--- hello/src/App.js
+++ hello/src/App.js
@@ -3,100 +3,107 @@ import logo from "./assets/favicon.ico";
 import { ThemeProvider } from "theme-ui";
 import theme from "./style/theme";
 import { Card, Image, Text, Box } from "theme-ui";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 var ReactRotatingText = require("react-rotating-text");
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div className="sidebar">
-          <img src={logo} hrf="/" alt="This is my favicon." />
-        </div>
-        <div className="header">
-          <Header />
-        </div>
-        <div className="wrapper">
-          <h1>
-            Hej, my name is
-            <ReactRotatingText
-              items={[
-                " julizet",
-                " juliazet",
-                " J.Z.",
-                " juliazet0",
-                " Julia Zet",
-                " Julia",
-              ]}
-            />
-          </h1>
-          <Box p={3} color="white" bg="primary">
-            Here comes a very long text:
-            {/* <marquee behavior="scroll" direction="left" scrollamount="10">
+        <Router>
+          <div className="sidebar">
+            <Link to="/">
+              <img src={logo} hrf="/" alt="This is my favicon." />
+            </Link>
+          </div>
+          <div className="header">
+            <Header />
+          </div>
+          <div className="wrapper">
+            <h1>
+              Hej, my name is
+              <ReactRotatingText
+                items={[
+                  " julizet",
+                  " juliazet",
+                  " J.Z.",
+                  " juliazet0",
+                  " Julia Zet",
+                  " Julia",
+                ]}
+              />
+            </h1>
+            <Box p={3} color="white" bg="primary">
+              Here comes a very long text:
+              {/* <marquee behavior="scroll" direction="left" scrollamount="10">
                 <p>There was a minimum of cinnamon in the aluminum pan.</p>
               </marquee> */}
-          </Box>
-          <p>
-            {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-            sagittis purus sit amet magna fermentum pharetra. Vivamus volutpat
-            ante in felis dapibus semper. Aenean suscipit metus tristique odio
-            sagittis, at pulvinar augue ultricies. Donec aliquet enim id
-            molestie cursus. Sed justo quam, rutrum eu egestas eget, hendrerit
-            convallis enim. Pellentesque vel nisi sit amet lorem aliquet
-            ultrices eget non quam. Suspendisse rutrum leo non sem ultrices,
-            vitae volutpat ante molestie. Nunc viverra tellus ex, id gravida
-            massa efficitur elementum. Proin non sem vel sem ultricies eleifend
-            sed a massa. Vivamus sed consectetur velit. Aliquam eu sollicitudin
-            metus, sit amet mollis tellus. Aliquam mattis gravida felis eget
-            tincidunt. Ut faucibus tortor eget fringilla rutrum. Fusce dictum
-            nisl eget dolor mollis tristique. Suspendisse potenti. Nullam ex
-            arcu, interdum id bibendum id, fringilla sit amet turpis.{" "}
-          </p>{" "}
-          <h1>Hej, Jude!</h1>
-          <Box p={3} color="white" bg="lightgrey">
-            Wanna play cards?
-          </Box>
-          <div className="cardWrapper">
-            <Card
-              sx={{
-                margin: 4,
-                maxWidth: 220,
-              }}
-            >
-              <Image src={logo} />
-              <Text sx={{ textAlign: "center" }}> Beep</Text>
-            </Card>
-            <Card
-              sx={{
-                margin: 4,
-                maxWidth: 220,
-              }}
-            >
-              <Image src={logo} />
-              <Text sx={{ textAlign: "center" }}> Meep</Text>
-            </Card>
-            <Card
-              sx={{
-                margin: 4,
-                maxWidth: 220,
-              }}
-            >
-              <Image src={logo} />
-              <Text sx={{ textAlign: "center" }}> Clunk</Text>
-            </Card>
-            <div className="columnOne"></div>
-          </div>
-          <div className="columnTwo">
-            <h1>Hej, Jude!</h1>
-            <Box p={4} color="white" bg="secondary">
-              Why not looking at my table?
             </Box>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              sagittis purus sit amet magna fermentum pharetra. Vivamus volutpat
+              ante in felis dapibus semper. Aenean suscipit metus tristique odio
+              sagittis, at pulvinar augue ultricies. Donec aliquet enim id
+              molestie cursus. Sed justo quam, rutrum eu egestas eget, hendrerit
+              convallis enim. Pellentesque vel nisi sit amet lorem aliquet
+              ultrices eget non quam. Suspendisse rutrum leo non sem ultrices,
+              vitae volutpat ante molestie. Nunc viverra tellus ex, id gravida
+              massa efficitur elementum. Proin non sem vel sem ultricies
+              eleifend sed a massa. Vivamus sed consectetur velit. Aliquam eu
+              sollicitudin metus, sit amet mollis tellus. Aliquam mattis gravida
+              felis eget tincidunt. Ut faucibus tortor eget fringilla rutrum.
+              Fusce dictum nisl eget dolor mollis tristique. Suspendisse
+              potenti. Nullam ex arcu, interdum id bibendum id, fringilla sit
+              amet turpis.{" "}
+            </p>{" "}
+            <h1>Hej, Jude!</h1>
+            <Box p={3} color="white" bg="lightgrey">
+              Wanna play cards?
+            </Box>
+            <div className="cardWrapper">
+              <Card
+                sx={{
+                  margin: 4,
+                  maxWidth: 220,
+                }}
+              >
+                <Image src={logo} />
+                <Text sx={{ textAlign: "center" }}> Beep</Text>
+              </Card>
+              <Card
+                sx={{
+                  margin: 4,
+                  maxWidth: 220,
+                }}
+              >
+                <Image src={logo} />
+                <Text sx={{ textAlign: "center" }}> Meep</Text>
+              </Card>
+              <Card
+                sx={{
+                  margin: 4,
+                  maxWidth: 220,
+                }}
+              >
+                <Image src={logo} />
+                <Text sx={{ textAlign: "center" }}> Clunk</Text>
+              </Card>
+              <div className="columnOne"></div>
+            </div>
+            <div className="columnTwo">
+              <h1>Hej, Jude!</h1>
+              <Box p={4} color="white" bg="secondary">
+                Why not looking at my table?
+              </Box>
+            </div>
           </div>
-        </div>
-        <Footer />
+          <Footer />
+        </Router>
       </ThemeProvider>
     );
   }
--- hello/src/components/Header.js
+++ hello/src/components/Header.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+
+import About from "./About";
+import Contact from "./Contact";
 
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar">
-        <a href="/">Profil</a>
-        <a href="/">Krams</a>
-        <a href="/">Kontakt</a>
+      <nav className="navbar topbar topbar-sticky">
+        <div className="topbar-right">
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />{" "}
+        </div>
       </nav>
     );
   }
