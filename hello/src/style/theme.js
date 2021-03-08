854d116099a21b112d338d8a51872a17f81ddb06
julizet <juliazet@gmx.de>
2021-03-08 22:18:37 +0100

    new: basic styling for theme UI

--- hello/src/style/theme.js
+++ hello/src/style/theme.js
@@ -1,23 +1,36 @@
 const theme = {
   breakpoints: ["40em", "56em", "64em", "128em"],
   fonts: {
-    body: "system-ui, sans-serif",
-    heading: '"Avenir Next", sans-serif',
+    body:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
     monospace: "Menlo, monospace",
   },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
   fontWeights: {
     body: 400,
     heading: 700,
+    bold: 700,
   },
   lineHeights: {
-    body: 1.5,
-    heading: 1.125,
+    body: 1.3,
+    heading: 1.5,
   },
   colors: {
     text: "#000",
     background: "#fff",
-    primary: "#FF2150",
-    secondary: "#3333ff",
+    primary: "#ff2150",
+    secondary: "#d0fedc",
+    muted: "#d3d3d3",
+    modes: {
+      dark: {
+        text: "#fff",
+        background: "#000",
+        primary: "#ff2150",
+        secondary: "#d0fedc",
+      },
+    },
   },
   cards: {
     primary: {
