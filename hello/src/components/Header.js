52e86a9e5e1bdc611146c15cbb9abb625bc37c19
julizet <juliazet@gmx.de>
2021-03-08 22:19:26 +0100

    new: color mode toggle at the top right

--- hello/src/components/Header.js
+++ hello/src/components/Header.js
@@ -1,25 +1,55 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
+import { useColorMode } from "theme-ui";
 
 import About from "./About";
 import Contact from "./Contact";
 
 const Header = () => {
+  const [colorMode, setColorMode] = useColorMode();
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to={"/about"} className="nav-link">
-          About
-        </Link>
-        <Link to={"/contact"} className="nav-link">
-          Contact
-        </Link>
-      </nav>
-      <Switch>
-        <Route path="/contact" component={Contact} />
-        <Route path="/about" component={About} />
-      </Switch>
+    <div
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      <div
+        sx={{
+          display: "inline-block",
+          textAlign: "center",
+          lineHeight: "inherit",
+          textDecoration: "none",
+          fontSize: "inherit",
+          fontWeight: "bold",
+          px: 3,
+          py: 2,
+        }}
+        onClick={(e) => {
+          setColorMode(colorMode === "default" ? "dark" : "default");
+        }}
+      >
+        {colorMode === "default" ? "Dark" : "Light"}
+      </div>
+      <div
+        sx={{
+          display: "inline-block",
+        }}
+      >
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to={"/about"} className="nav-link">
+            About
+          </Link>
+          <Link to={"/contact"} className="nav-link">
+            Contact
+          </Link>
+        </nav>
+        <Switch>
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
     </div>
   );
 };
