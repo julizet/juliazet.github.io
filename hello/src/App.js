80b4817ed42aeb7d87ca66827378f6ba7d70f84a
julizet <juliazet@gmx.de>
2021-04-22 13:06:20 +0200

    add overlay + modal

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import logo from "./assets/favicon.ico";
+import hello from "./assets/hello.png";
 import { ThemeProvider } from "theme-ui";
 import theme from "./style/theme";
 import { Link } from "react-router-dom";
@@ -15,21 +16,33 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div className="wrapper">
-          <div className="sidebar">
-            <Link className="nav-link" to="/">
-              <img src={logo} alt="This is my favicon." />
-            </Link>
-          </div>
-          <div className="header">
-            <Header />
-          </div>
-          <div className="content">
-            <Rotation />
-            <SectionCivic />
-            <SectionOpenSource />
-            <SectionPrototyping />
-            <Footer />
+        <div class="modal">
+          <img
+            style={{ maxWidth: 40 + "px" }}
+            src={hello}
+            alt="This is my hello hand."
+          />
+          <h4 style={{ marginTop: -10 + "em" }}>
+            This website is under construction
+          </h4>
+        </div>
+        <div className="overlay">
+          <div className="wrapper">
+            <div className="sidebar">
+              <Link className="nav-link" to="/">
+                <img src={logo} alt="This is my favicon." />
+              </Link>
+            </div>
+            <div className="header">
+              <Header />
+            </div>
+            <div className="content">
+              <Rotation />
+              <SectionCivic />
+              <SectionOpenSource />
+              <SectionPrototyping />
+              <Footer />
+            </div>
           </div>
         </div>
       </ThemeProvider>
